9b47dc965f162612703c09f487f6a7e19a19ee41
Xuderek01 <epnightmare02@gmail.com>
2021-04-08 11:02:28 -0400

    Added validateCreditCard Method
    Adding method that is used to validate the credit cards that the user is inputting

--- CustomerSystem.java
+++ CustomerSystem.java
@@ -90,7 +90,113 @@ class CustomerSystem{
             }
         
     
+    
     public static void generateCustomerDataFile(){
+    }
+
+    public static boolean validateCreditCard(){
+        Scanner sc = new Scanner(System.in); //Initializes all the variables that are needed in the card validation
+        boolean cardDigitCheck = false; //These are local variables 
+        String reverseCard = "";
+        //String creditCard = ""; No longer need this. Was used to test the code by itself 
+        String digit = "";
+        int cardNumber = 0;
+        int sum1 = 0;
+        String newNum = "";
+        String digit1String = "";
+        String digit2String = "";
+        int digit1 = 0;
+        int digit2 = 0;
+        int digitSum = 0;
+        int sum2 = 0;
+        int totalSum = 0;
+        String totalSumString = "";
+
+        try{  //Try and catch loop just in case used inputs invalid credit card and causes an error to occur
+            while (cardDigitCheck != true){ //While loop to make sure user inputs the correct number of digits
+                /*System.out.print("Please enter credit card number (No spaces): ");
+                 creditCard = sc.nextLine(); */ //Was used in order to test the credit card validation code
+
+                int creditCardLength = creditCardNum.length(); //Checks the digits of the credit card
+
+                if (creditCardLength != 9){ //If the card is not equal to 9 digits, will reloop
+                    System.out.println("\nPlease enter a card with a valid number of digits.\n");
+                }
+
+                else { //Releases the loop if the card has exactly 9 digits
+                    cardDigitCheck = true;
+                }
+
+            }
+
+            for (int i = 8; i >= 0; i --){ //Reverses the digits of the card
+                reverseCard = reverseCard + creditCardNum.charAt(i);
+            }
+
+
+            for (int j = 0; j <= 8; j += 2){
+                digit = digit + reverseCard.charAt(j);
+                cardNumber = Integer.parseInt(digit);
+                sum1 = sum1 + cardNumber;
+                digit = "";
+            } 
+
+            for (int k = 1; k <= 7; k += 2){
+                digit = digit + reverseCard.charAt(k);
+                cardNumber = Integer.parseInt(digit);
+                cardNumber = cardNumber * 2;
+                newNum = String.valueOf(cardNumber);
+                if (newNum.length() == 2){
+                    digit1String = digit1String + newNum.charAt(0);
+                    digit2String = digit2String + newNum.charAt(1);
+                    digit1 = Integer.parseInt(digit1String);
+                    digit2 = Integer.parseInt(digit2String);
+                    digitSum = digit1 + digit2;
+                    sum2 = sum2 + digitSum;
+                    digitSum = 0;
+                    digit1String = "";
+                    digit2String = "";
+                    digit1 = 0;
+                    digit2 = 0;
+                }
+                else{
+                    sum2 = sum2 + cardNumber;
+                }
+                    
+                digit = "";
+            }
+
+            totalSum = sum1 + sum2;
+
+            totalSumString = String.valueOf(totalSum);
+            digit1String = digit1String + totalSumString.charAt(0);
+            if (totalSumString.length() == 2){
+                digit2String = digit2String + totalSumString.charAt(1);
+                digit2 = Integer.parseInt(digit2String);
+                if (digit2 == 0){ //Change this to boolean first
+                    System.out.println("valid");
+                }
+                else{
+                    System.out.println("Not valid");
+                }
+            }
+            else{
+                digit1 = Integer.parseInt(digit1String);
+
+                if (digit1 == 0){ //Change to boolean first
+                    System.out.println("valid");
+                }
+                else{
+                    System.out.println("Not valid");
+                }
+
+            }
+
+        }
+
+        catch(Exception e){
+            System.out.println("Something went wrong");
+        }
     }
 
 }
